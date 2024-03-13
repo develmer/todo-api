2f69abc6e89f4c2a4ee39c642fb9bf49a305c9fe
develmer <devinatme@gmail.com>
2024-03-14 02:57:04 +0800

    change value of database connection variable

--- server/database/database.connection.ts
+++ server/database/database.connection.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import dotenv from 'dotenv';
 dotenv.config();
 
-let config;
+let config: object;
 if (process.env.NODE_ENV == "dev") {
     config = {
         host: process.env.POSTGRES_HOST_DEV,
